b87cc0014300b0ddb4d4f801775f545f18ffd98d
B23-User <ikramazim@hotmail.com>
2022-01-13 11:06:04 -0500

    Car Class

--- day38_Inheritance/carTask/Toyota.java
+++ day38_Inheritance/carTask/Toyota.java
@@ -1,11 +1,19 @@
 package day38_Inheritance.carTask;
 
 public class Toyota extends Car {
-    public Toyota(String brand, String model, int year, double price, String color, int miles) {
-        super( "Toyota", model, year, price, color, miles);
+
+    public Toyota( String model, int year, double price, String color, int miles) {
+        super("Toyota", model, year, price, color, miles);
     }
 
     public void reliable(){
-        System.out.println(brand+ " " +model+ " is reliable");
+        System.out.println(brand+" "+model+" is reliable");
     }
+
 }
+
+/*
+1. Toyota:
+					extra methods:
+							reliable()
+ */
